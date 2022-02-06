ebc501433d1f9ef5bed81e428e3da9bc1edc2ae0
Mikhail Potapov <86321783+mishapotap@users.noreply.github.com>
2022-02-06 22:19:06 +0300

    Store_enchancers_and_compose_used

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,27 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, compose } from "redux";
 import heroes from "../reducers/heroes";
 import filters from "../reducers/filters";
 
+const enchancer =
+    (createStore) =>
+    (...args) => {
+        const store = createStore(...args);
+
+        const oldDispatch = store.dispatch;
+        store.dispatch = (action) => {
+            if (typeof action === "string") {
+                return oldDispatch({
+                    type: action,
+                });
+            }
+            return oldDispatch(action);
+        };
+        return store;
+    }; // store enchancer (Усилитель стора)
+
 const store = createStore(
     combineReducers({ heroes, filters }),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    compose(enchancer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 ); // Сократили {heroes: heroes, filters: filters}
 
 export default store;
