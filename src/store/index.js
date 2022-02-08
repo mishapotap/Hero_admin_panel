bd1f5476ebc83af79aa45fa07993d9dba8c606e6
Mikhail Potapov <86321783+mishapotap@users.noreply.github.com>
2022-02-08 10:24:49 +0300

    MiddleWare

--- src/store/index.js
+++ src/store/index.js
@@ -1,27 +1,39 @@
-import { createStore, combineReducers, compose } from "redux";
+import { createStore, combineReducers, compose, applyMiddleware } from "redux";
 import heroes from "../reducers/heroes";
 import filters from "../reducers/filters";
 
-const enchancer =
-    (createStore) =>
-    (...args) => {
-        const store = createStore(...args);
+const stringMiddleware = (store) => (dispatch) => (action) => {
+    if (typeof action === "string") {
+        return dispatch({
+            type: action,
+        });
+    }
+    return dispatch(action);
+};
 
-        const oldDispatch = store.dispatch;
-        store.dispatch = (action) => {
-            if (typeof action === "string") {
-                return oldDispatch({
-                    type: action,
-                });
-            }
-            return oldDispatch(action);
-        };
-        return store;
-    }; // store enchancer (Усилитель стора)
+// const enchancer =
+//     (createStore) =>
+//     (...args) => {
+//         const store = createStore(...args);
+
+//         const oldDispatch = store.dispatch;
+//         store.dispatch = (action) => {
+//             if (typeof action === "string") {
+//                 return oldDispatch({
+//                     type: action,
+//                 });
+//             }
+//             return oldDispatch(action);
+//         };
+//         return store;
+//     }; // store enchancer (Усилитель стора)
 
 const store = createStore(
     combineReducers({ heroes, filters }),
-    compose(enchancer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    compose(
+        applyMiddleware(stringMiddleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
 ); // Сократили {heroes: heroes, filters: filters}
 
 export default store;
