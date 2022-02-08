dc83adfb0e004cf617973f869fc8843e20df1744
Mikhail Potapov <86321783+mishapotap@users.noreply.github.com>
2022-02-08 11:56:27 +0300

    Redux_toolkit_used

--- src/store/index.js
+++ src/store/index.js
@@ -1,40 +1,28 @@
-import { createStore, combineReducers, compose, applyMiddleware } from "redux";
-import ReduxThunk from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
 import heroes from "../reducers/heroes";
 import filters from "../reducers/filters";
 
-// const stringMiddleware = (store) => (dispatch) => (action) => {
-//     if (typeof action === "string") {
-//         return dispatch({
-//             type: action,
-//         });
-//     }
-//     return dispatch(action);
-// };
+const stringMiddleware = () => (next) => (action) => {
+    if (typeof action === "string") {
+        return next({
+            type: action,
+        });
+    }
+    return next(action);
+};
 
-// const enchancer =
-//     (createStore) =>
-//     (...args) => {
-//         const store = createStore(...args);
+// const store = createStore(
+//     combineReducers({ heroes, filters }),
+//     compose(
+//         applyMiddleware(ReduxThunk, stringMiddleware),
+//         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//     )
+// );
 
-//         const oldDispatch = store.dispatch;
-//         store.dispatch = (action) => {
-//             if (typeof action === "string") {
-//                 return oldDispatch({
-//                     type: action,
-//                 });
-//             }
-//             return oldDispatch(action);
-//         };
-//         return store;
-//     }; // store enchancer (Усилитель стора)
-
-const store = createStore(
-    combineReducers({ heroes, filters }),
-    compose(
-        applyMiddleware(ReduxThunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-); // Сократили {heroes: heroes, filters: filters}
+const store = configureStore({
+    reducer: { heroes, filters },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(stringMiddleware),
+    devTools: process.env.NODE_ENV !== "production",
+});
 
 export default store;
